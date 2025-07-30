c190901747160d56db8e688eb52832a09829acc0
gnohnoey <mj9514884@gmail.com>
2025-07-30 10:37:26 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const App = () => {
+  const name = "gnohnoey";
+
+  return ( 
+    <>
+      <div className="card">
+        <img src="./avatar.webp" alt="avatar" />
+        <h2>gnohnoey</h2>
+        <h2>Programmer</h2>
+      </div>
+    </>
+  );
+};
+
+export default App;
